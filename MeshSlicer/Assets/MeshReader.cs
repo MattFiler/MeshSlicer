6dea881205cdddeee3bee47d841cc623634254ab
MattFiler <matthew.filer@hotmail.co.uk>
2019-11-11 17:46:17 +0000

    Added configs for building to Quest

---
+++ MeshSlicer/Assets/MeshReader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshReader : MonoBehaviour
+{
+    private Mesh theMesh;
+    private MeshFilter theMeshFilter;
+    void Start()
+    {
+        theMesh = GetComponent<Mesh>();
+        theMeshFilter = GetComponent<MeshFilter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //theMeshFilter.mesh.Optimize
+    }
+}
