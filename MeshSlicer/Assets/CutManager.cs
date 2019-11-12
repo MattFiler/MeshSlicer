f3e14b3ec7fe36d777fd5dc0ef5e1b9e82472bab
MattFiler <matthew.filer@hotmail.co.uk>
2019-11-12 13:07:20 +0000

    Click a triangle to remove it

---
+++ MeshSlicer/Assets/CutManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutManager : MonoBehaviour
+{
+    private LineRenderer laserLineRenderer;
+    private void Start()
+    {
+        laserLineRenderer = GetComponent<LineRenderer>();
+        Vector3[] initLaserPositions = new Vector3[2] { Vector3.zero, Vector3.zero };
+        laserLineRenderer.SetPositions(initLaserPositions);
+        laserLineRenderer.startWidth = 0.5f;
+        laserLineRenderer.endWidth = 0.5f;
+    }
+
+    [SerializeField] private GameObject vrHand;
+
+    /* On PC, allow mouse clicks to interact with a mesh, in VR do it from controller */
+    void Update()
+    {
+        //PC click interaction
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 1000.0f))
+            {
+                DeleteTriangle(hit.triangleIndex, hit.transform.gameObject);
+            }
+
+            laserLineRenderer.SetPosition(0, ray.origin);
+            laserLineRenderer.SetPosition(1, ray.origin + (1000.0f * ray.direction));
+            laserLineRenderer.enabled = true;
+        }
+
+        //VR point interaction
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(vrHand.transform.position, vrHand.transform.forward);
+            if (Physics.Raycast(ray, out hit, 1000.0f))
+            {
+                DeleteTriangle(hit.triangleIndex, hit.transform.gameObject);
+            }
+
+            laserLineRenderer.SetPosition(0, ray.origin);
+            laserLineRenderer.SetPosition(1, ray.origin + (1000.0f * ray.direction));
+            laserLineRenderer.enabled = true;
+        }
+
+        //Mouse interaction lock
+        if (Input.GetKey(KeyCode.Space))
+            Cursor.lockState = CursorLockMode.None;
+        else
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    /* Delete a triangle at a given index in a given GameObject's mesh */
+    private void DeleteTriangle(int index, GameObject obj)
+    {
+        if (!obj.GetComponent<MeshCollider>()) return;
+
+        Destroy(obj.GetComponent<MeshCollider>());
+        Mesh mesh = obj.transform.GetComponent<MeshFilter>().mesh;
+        int[] oldTriangles = mesh.triangles;
+        int[] newTriangles = new int[mesh.triangles.Length - 3];
+
+        int i = 0;
+        int j = 0;
+        while (j < mesh.triangles.Length)
+        {
+            if (j != index * 3)
+            {
+                newTriangles[i++] = oldTriangles[j++];
+                newTriangles[i++] = oldTriangles[j++];
+                newTriangles[i++] = oldTriangles[j++];
+            }
+            else
+            {
+                j += 3;
+            }
+        }
+
+        obj.transform.GetComponent<MeshFilter>().mesh.triangles = newTriangles;
+        obj.AddComponent<MeshCollider>();
+    }
+}
---
+++ MeshSlicer/Assets/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector2 rotation = new Vector2(0, 0);
+    public float speed = 3;
+
+    /* Basic player movement for PC/VR */
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.Up) || Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += this.transform.forward * Time.deltaTime;
+        }
+        if (OVRInput.Get(OVRInput.Button.Down) || Input.GetKey(KeyCode.S))
+        {
+            this.transform.position -= this.transform.forward * Time.deltaTime;
+        }
+        if (OVRInput.Get(OVRInput.Button.Left) || Input.GetKey(KeyCode.A))
+        {
+            this.transform.position -= this.transform.right * Time.deltaTime;
+        }
+        if (OVRInput.Get(OVRInput.Button.Right) || Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += this.transform.right * Time.deltaTime;
+        }
+
+        //If no controllers are connected, we're probs not in VR, so look around with mouse
+        if (!OVRInput.IsControllerConnected(OVRInput.Controller.All))
+        {
+            rotation.y += Input.GetAxis("Mouse X");
+            rotation.x += -Input.GetAxis("Mouse Y");
+            transform.eulerAngles = (Vector2)rotation * speed;
+        }
+    }
+}
