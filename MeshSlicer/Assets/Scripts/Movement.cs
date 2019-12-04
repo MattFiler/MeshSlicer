0484e836500685c8260c79cc4ec2ed8f42360284
MattFiler <matthew.filer@hotmail.co.uk>
2019-12-04 18:19:07 +0000

    Camera speed modifier

--- MeshSlicer/Assets/Scripts/Movement.cs
+++ MeshSlicer/Assets/Scripts/Movement.cs
@@ -6,25 +6,35 @@ public class Movement : MonoBehaviour
 {
     Vector2 rotation = new Vector2(0, 0);
     public float speed = 3;
+    private float modifier = 1;
 
     /* Basic player movement for PC/VR */
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            modifier = 3.0f;
+        }
+        else
+        {
+            modifier = 1.0f;
+        }
+
         if (OVRInput.Get(OVRInput.Button.Up) || Input.GetKey(KeyCode.W))
         {
-            this.transform.position += this.transform.forward * Time.deltaTime;
+            this.transform.position += this.transform.forward * Time.deltaTime * modifier;
         }
         if (OVRInput.Get(OVRInput.Button.Down) || Input.GetKey(KeyCode.S))
         {
-            this.transform.position -= this.transform.forward * Time.deltaTime;
+            this.transform.position -= this.transform.forward * Time.deltaTime * modifier;
         }
         if (OVRInput.Get(OVRInput.Button.Left) || Input.GetKey(KeyCode.A))
         {
-            this.transform.position -= this.transform.right * Time.deltaTime;
+            this.transform.position -= this.transform.right * Time.deltaTime * modifier;
         }
         if (OVRInput.Get(OVRInput.Button.Right) || Input.GetKey(KeyCode.D))
         {
-            this.transform.position += this.transform.right * Time.deltaTime;
+            this.transform.position += this.transform.right * Time.deltaTime * modifier;
         }
 
         //If no controllers are connected, we're probs not in VR, so look around with mouse
